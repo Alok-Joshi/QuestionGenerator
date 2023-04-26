32a8aafb2d846ef7b3d804a52f64c26b4e6a5742
Alok Joshi <AlokJoshi523@gmail.com>
2023-04-26 23:35:05 +0530

    Skeleton of distractor implemented

---
+++ distractor.py
@@ -0,0 +1,36 @@
+
+
+class distractor:
+
+    def __init__(self,input_text):
+        
+        self.input_text = input_text;
+
+
+    def initialise(self):
+    
+        #preprocess the text
+        #generate the tdm
+        pass
+
+    def _preprocess_text(self):
+
+        #preprocess the text here
+        pass
+
+
+    def _generate_tdm(self):
+
+        #
+        pass
+
+    def get_similar_words(self,word,k):
+        #returns k similar words
+        pass
+    
+    def _cosine_similarity(self,word1,word2):
+        #
+        pass
+
+
+
